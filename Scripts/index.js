e5ac8b51f4648b65df63d6a8339c295ede6efa47
miguel Areiza <you@example.com>
2023-05-04 21:48:07 -0500

    Cupor y familia

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,7 +5,18 @@ $(document).ready( () => {
     $('#btnAddress').click( () => {
 
         window.open('https://goo.gl/maps/N6tsnZdnDGwi2vhf8');
-        
+
     });
+
+    const URLPARAMS = new URLSearchParams(window.location.search);
+    
+    if (URLPARAMS.has('familia') && URLPARAMS.has('cupo')) {
+        
+        const FAMILIA = URLPARAMS.get('familia');
+        const CUPO = URLPARAMS.get('cupo');
+
+        $('.invitacion').html(`FAMILIA <b>${FAMILIA}</b>, TENEMOS EL HONOR DE INVITARTE A NUESTRA BODA TE HEMOS RESERVADO ${CUPO} CUPOS`);
+
+    }
 
 });
