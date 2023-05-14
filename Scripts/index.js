299b5fc17e3475e0c2a7143cfbd7ab8650ce2928
miguel Areiza <you@example.com>
2023-05-14 01:56:50 -0500

    confirmacion

--- Scripts/index.js
+++ Scripts/index.js
@@ -16,13 +16,8 @@ $(document).ready( () => {
         }
     });
     
-    $('#btnAddress').click( () => {
-
-        window.open('https://goo.gl/maps/N6tsnZdnDGwi2vhf8');
-
-    });
-
     const URLPARAMS = new URLSearchParams(window.location.search);
+    let invitados = '';
     
     if (URLPARAMS.has('invitados')) {
         
@@ -33,11 +28,26 @@ $(document).ready( () => {
         for (let i = 0; i < INVITADOS.length; i++) {
 
             html += `<hr><b>${INVITADOS[i]}</b>`;
+            invitados += `${INVITADOS[i]}\n`
             
         }
 
         $('.invitacion').html(`TENEMOS EL HONOR DE INVITARTE A NUESTRA BODA TE HEMOS RESERVADO ${INVITADOS.length} CUPOS PARA: ${html}`);
 
     }
+    
+    $('#btnAddress').click( () => {
+
+        window.open('https://goo.gl/maps/N6tsnZdnDGwi2vhf8');
+
+    });
+
+    $('#btnConfirm').click( () => {
+
+        const telefono = '573218188612';
+        const mensaje = `Confirmamos la asistencia de:\n ${invitados} ¡nos veremos allá!`;
+        window.open(`https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`);
+
+    });
 
 });
