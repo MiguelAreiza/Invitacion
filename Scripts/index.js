214d58c0f57e1c9c519051e001bbd78c78272c96
miguel Areiza <you@example.com>
2023-05-14 01:13:33 -0500

    sound

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,18 +3,18 @@
 $(document).ready( () => {
 
     let sound = new Audio('../assets/Sound.mp3');
-    
-    Swal.fire({
-        title: '¿Deseas reproducir el sonido?',
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonText: 'Sí',
-        cancelButtonText: 'No'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            sound.play();
-        }
-    });
+    sound.play()
+    // Swal.fire({
+    //     title: '¿Deseas reproducir el sonido?',
+    //     icon: 'question',
+    //     showCancelButton: true,
+    //     confirmButtonText: 'Sí',
+    //     cancelButtonText: 'No'
+    // }).then((result) => {
+    //     if (result.isConfirmed) {
+    //         sound.play();
+    //     }
+    // });
     
     $('#btnAddress').click( () => {
 
